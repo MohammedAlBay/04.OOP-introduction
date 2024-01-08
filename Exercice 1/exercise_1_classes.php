46585d9940c820d4751fd4ec2074db9472797826
MohammedAB <albay.m@hotmail.com>
2024-01-08 16:43:08 +0100

    Add solution to exercice 1

---
+++ Exercice 1/exercise_1_classes.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+/* EXERCISE 1
+TODO: Create a class beverage.
+TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
+TODO: Have a default value "cold" in the construct for temperature.
+
+Remember for now we will use properties and methods that can be accessed from everywhere.
+TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
+TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
+ print the getInfo on the screen.
+TODO: Print the temperature on the screen.
+
+USE TYPEHINTING EVERYWHERE!
+*/
+
+class Beverage
+{
+  // Properties
+  public string $color;
+  public float $price;
+  public string $temperature;
+
+  // Construct
+  public function __construct(string $color, float $price, $defaultTemperature = "cold")
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->temperature = $defaultTemperature;
+  }
+
+  // The function
+  public function getInfo() 
+  {
+    return "This beverage is $this->temperature and $this->color and costs &euro;$this->price.";
+  }
+}
+
+$cola = new Beverage("black", 2);
+echo $cola->getInfo();
+echo "<p>Temperature: $cola->temperature</p>";
+
+// require "view-exercice-1.php";
