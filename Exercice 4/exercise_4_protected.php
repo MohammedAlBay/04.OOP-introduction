2ed8ead87897aec2d11b96e72a11354d3d8d5125
MohammedAB <albay.m@hotmail.com>
2024-01-10 04:33:14 +0100

    Copy code from previous exercice

--- Exercice 4/exercise_4_protected.php
+++ Exercice 4/exercise_4_protected.php
@@ -11,3 +11,26 @@ TODO: Make all the other prints work without error without changing the beverage
 
 USE TYPEHINTING EVERYWHERE!
 */
+
+include_once "Beverage.php";
+include_once "Beer.php";
+
+// Instantiate a new object called "Duvel" with the use of the construct
+$Duvel = new Beer("blond", 3.5, "Duvel", 8.5);
+
+// One way of printing method
+echo "Alcohol percentage of Duvel is: " . $Duvel->get_AlcoholPercentage() . "%";
+echo "<br>";
+// Alternative way of printing the method by assigning it to a new variable & echo said variable
+$alcoholAmount = $Duvel->get_AlcoholPercentage();
+echo "Alcohol percentage of Duvel is: " . $alcoholAmount . "%";
+echo "<br>";
+echo "Color of Duvel is: " . $Duvel->get_Color();
+echo "<br>";
+echo $Duvel->getInfo();
+echo "<br>";
+
+$Duvel->set_Color("light");
+echo "New color of Duvel is: " . $Duvel->get_Color();
+echo "<br>";
+echo $Duvel->get_beerInfo();
