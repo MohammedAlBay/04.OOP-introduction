4f1f7d9fcf20c74897d3b6b96f967d04f94f5ee9
MohammedAB <albay.m@hotmail.com>
2024-01-10 09:23:40 +0100

    Move code into Beverage file and use include instead

--- Exercice 5/exercise_5_public.php
+++ Exercice 5/exercise_5_public.php
@@ -11,24 +11,4 @@ TODO: Fix the errors without using getter and setter functions.
 TODO: Change the price to 3.5 euro and print it also on the screen on a new line.
 */
 
-class Beverage
-{
-  // Properties
-  private string $color;
-  private float $price;
-  private string $temperature;
-
-  // Construct
-  public function __construct(string $color, float $price)
-  {
-    $this->color = $color;
-    $this->price = $price;
-    $this->temperature = "cold";
-  }
-
-  // The function
-  public function getInfo() 
-  {
-    return "This beverage is $this->temperature, has a $this->color color and costs &euro;$this->price.";
-  }
-}
+include_once "Beverage.php";
